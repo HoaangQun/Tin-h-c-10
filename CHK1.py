397b6b3967c0a40164ea126499d2c09ccf5f17e6
HoaangQun <hoangquanken97@gmail.com>
2025-01-11 12:43:28 +0700

    tự luận cuối hkI

---
+++ CHK1.py
@@ -0,0 +1,8 @@
+#tính diện tích và chu vi hình chữ nhật
+r,d = map(float, input("Nhập vào chiều dài/rộng hcn: ").split())
+print( f"Chu vi hình chữ nhật = {(r+d)*2}")
+print( f"Diện tích hình chữ nhật = {r*d}")
+
+#tính tổng bình phương
+a,b = map(float, input("Nhập vào 2 số: ").split())
+print( f"Tổng bình phương 2 số = {a**2+b**2}")
